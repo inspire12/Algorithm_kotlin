5038bdeb501489eac8f73ac4eb73073cac2871b2
inspire12 <ox4443@naver.com>
2024-12-24 12:37:52 +0900

    first commit

---
+++ src/main/kotlin/algorithm/MathUtils.kt
@@ -0,0 +1,11 @@
+package com.inspire12.algorithm
+
+class Math {
+    fun isPrime(n: Int): Boolean {
+        if (n < 2) return false
+        for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
+            if (n % i == 0) return false
+        }
+        return true
+    }
+}
---
+++ src/test/kotlin/solver/programmers/pccp1/SolutionTest.kt
@@ -0,0 +1,5 @@
+package solver.programmers.pccp1
+
+import org.junit.jupiter.api.Assertions.*
+
+class SolutionTest
